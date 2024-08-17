09ea57e4f6f5989d4737394c234864853b4027d9
Germano Kuerten <germanokuerten@gmail.com>
2024-08-16 22:07:57 -0300

    Fetch the genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,13 +1,21 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  SimpleGrid,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card>
-      <Skeleton height="200px" />
-      <CardBody>
-        <SkeletonText />
-      </CardBody>
-    </Card>
+
+      <Card>
+        <Skeleton height="200px" />
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+      </Card>
+
   );
 };
 
