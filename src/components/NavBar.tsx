95f335312ed2d5b3da3c10135b4a2cdea6ed1f8a
Germano Kuerten <germanokuerten@gmail.com>
2024-08-18 17:50:20 -0300

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,16 @@ import logo from '../assets/OnDaRox_Logo.png'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
         <Image src={logo} boxSize='60px' margin='7px' borderRadius='15px'></Image>
 
-        <SearchInput />
+        <SearchInput onSearch={onSearch}/>
         <ColorModeSwitch />
     </HStack>
   )
